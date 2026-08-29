d6b1b427729534667014d11b90620f00064972b8
soayngoc02-cpu <soayngoc02@gmail.com>
2026-08-29 23:13:17 +0700

    Bump NGS Music Visualizer Pro to 2.1

--- android-visualizer/app/build.gradle.kts
+++ android-visualizer/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ngsmusic.visualizer"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0-pro"
+        versionCode = 3
+        versionName = "2.1-pro"
     }
 
     buildTypes {
