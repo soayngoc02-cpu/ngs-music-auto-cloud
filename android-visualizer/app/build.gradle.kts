f8693efc026969fdb95e93706f54fe9d81fe5c42
soayngoc02-cpu <soayngoc02@gmail.com>
2026-08-29 22:54:28 +0700

    Pro: bump app version and use new exporter

--- android-visualizer/app/build.gradle.kts
+++ android-visualizer/app/build.gradle.kts
@@ -11,8 +11,14 @@ android {
         applicationId = "com.ngsmusic.visualizer"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0-pro"
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.exclude("**/VideoExporter.kt")
+        }
     }
 
     buildTypes {
